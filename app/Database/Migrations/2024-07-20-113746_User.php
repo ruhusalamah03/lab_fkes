b76e50c058537e601b7952adf206b580165545b2
ssykra <rambesarah03@gmail.com>
2024-07-20 19:17:53 +0700

    tambah migrasi

---
+++ app/Database/Migrations/2024-07-20-113746_User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class User extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_user'           => [
+                'type'              => 'BIGINT',
+                'constraint'        => 20,
+                'unsigned'          => true,
+                'auto_increment'    => true,
+            ],
+            'name_user'         => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '50',
+            ],
+            'nim'               => [
+                'type'              => 'INT',
+                'constraint'        => '15',
+            ],
+            'password_user'     => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '60',
+            ],     
+        ]);
+        $this->forge->addKey('id_user', true);
+        $this->forge->createTable('login');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('login');
+    }
+}
